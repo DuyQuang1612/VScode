051ef797892d06faf4c61bcb7fa2f5d6e3874ac3
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-12-13 22:44:38 +0700

    .

---
+++ elvys/elvys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace elvys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
